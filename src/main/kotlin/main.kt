106dd5a07b56a46ac329eb9565418245cfdf5542
tantico <tantico@hotmail.com>
2022-04-21 23:00:34 +0300

    dz-2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,4 @@
+const val KOPEYKA = 100U
 const val CARD_VK = 1U
 const val CARD_MIR = 2U
 const val CARD_VISA = 3U
@@ -6,13 +7,17 @@ const val CARD_MS = 5U
 
 fun main() {
     print("Сумма перевода: ")
-    val sum = readln().toUInt() * 100U;
+    val sum = readln().toUInt() * KOPEYKA;
 
-    val card = getCard()
-    val lmSum = getLastMonthSum()
+    println("Коды карт: VKPay - $CARD_VK, MIR - $CARD_MIR, VISA - $CARD_VISA, Master - $CARD_MC, Maestro - $CARD_MS")
+    print("код вашей карты (по умолчанию VKPay): ")
+    val card = getCard(readln())
+
+    print("Сумма переводов за прошлый месяц (по умолчанию 0): ")
+    val lmSum = getLastMonthSum(readln())
 
     println()
-    println("Комиссия: ${fee(sum, card, lmSum)}")
+    println("Комиссия: ${fee(sum, card, lmSum) / KOPEYKA}")
 }
 
 fun fee(sum: UInt, card: UInt = CARD_VK, lastMonthSum: UInt = 0U): UInt {
@@ -37,17 +42,11 @@ fun fee(sum: UInt, card: UInt = CARD_VK, lastMonthSum: UInt = 0U): UInt {
 
     }
 
-    return result / 100U
+    return result
 }
 
-
-fun getCard(): UInt {
-    println("Коды карт: VKPay - $CARD_VK, MIR - $CARD_MIR, VISA - $CARD_VISA, Master - $CARD_MC, Maestro - $CARD_MS")
-    print("код вашей карты (по умолчанию VKPay): ")
-
-    var inp : String = readLine()?.trim() ?: ""
-
-    val card = if (inp.trim() == "") CARD_VK else inp.toUInt()
+fun getCard(input: String): UInt {
+    val card = if (input.trim() == "") CARD_VK else input.toUInt()
 
     return if ((card < CARD_VK) || (card > CARD_MS))
         0U
@@ -55,12 +54,9 @@ fun getCard(): UInt {
         card
 }
 
-fun getLastMonthSum(): UInt {
-    print("Сумма переводов за прошлый месяц (по умолчанию 0): ")
-    val inp : String = readLine()?.trim() ?: ""
-
-    return if (inp == "")
+fun getLastMonthSum(input: String): UInt {
+    return if (input == "")
         0U
     else
-        inp.toUInt() * 100U
+        input.toUInt() * KOPEYKA
 }
---
+++ src/test/java/MainKtTest.kt
@@ -0,0 +1,146 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MainKtTest {
+
+    @Test
+    fun fee_0_on_vkcard() {
+        //arrange
+        val sum = 1_000U * KOPEYKA
+        val card = CARD_VK
+        val lastMonthSum = 0U
+        val expectedValue = 0U
+
+        //act
+        val actualValue = fee(sum, card, lastMonthSum)
+
+        //assert
+        assertEquals(expectedValue, actualValue)
+    }
+
+    @Test
+    fun fee_35_on_mir() {
+        //arrange
+        val sum = 100U * KOPEYKA
+        val card = CARD_MIR
+        val lastMonthSum = 0U
+        val expectedValue = 35U * KOPEYKA
+
+        //act
+        val actualValue = fee(sum, card, lastMonthSum)
+
+        //assert
+        assertEquals(expectedValue, actualValue)
+    }
+
+    @Test
+    fun fee_big_fee_on_mir() {
+        //arrange
+        val sum = 10_000U * KOPEYKA
+        val card = CARD_MIR
+        val lastMonthSum = 0U
+        val expectedValue = 35U * KOPEYKA
+
+        //act
+        val actualValue = fee(sum, card, lastMonthSum)
+
+        //assert
+        assertTrue(expectedValue < actualValue)
+    }
+
+    @Test
+    fun fee_mastercard_less_75() {
+        //arrange
+        val sum = 1_000U * KOPEYKA
+        val card = CARD_MC
+        val lastMonthSum = 100U * KOPEYKA
+        val expectedValue = 0U
+
+        //act
+        val actualValue = fee(sum, card, lastMonthSum)
+
+        //assert
+        assertEquals(expectedValue, actualValue)
+    }
+
+    @Test
+    fun fee_mastercard_more_75() {
+        //arrange
+        val sum = 1_000U * KOPEYKA
+        val card = CARD_MC
+        val lastMonthSum = 75_000U * KOPEYKA
+        val expectedValue = 0U
+
+        //act
+        val actualValue = fee(sum, card, lastMonthSum)
+
+        //assert
+        assertTrue(expectedValue < actualValue)
+    }
+
+    @Test
+    fun getCard_default() {
+        //arrange
+        val input = ""
+        val expectedValue = CARD_VK
+
+        //act
+        val actualValue = getCard(input)
+
+        //assert
+        assertEquals(expectedValue, actualValue)
+    }
+
+    @Test
+    fun getCard_zero_min() {
+        //arrange
+        val input = "0"
+        val expectedValue = 0U
+
+        //act
+        val actualValue = getCard(input)
+
+        //assert
+        assertEquals(expectedValue, actualValue)
+    }
+
+    @Test
+    fun getCard_zero_max() {
+        //arrange
+        val input = "10"
+        val expectedValue = 0U
+
+        //act
+        val actualValue = getCard(input)
+
+        //assert
+        assertEquals(expectedValue, actualValue)
+    }
+
+    @Test
+    fun getLastMonthSum_default() {
+        //arrange
+        val input = ""
+        val expectedValue = 0U
+
+        //act
+        val actualValue = getLastMonthSum(input)
+
+        //assert
+        assertEquals(expectedValue, actualValue)
+    }
+
+    @Test
+    fun getLastMonthSum_no_default() {
+        //arrange
+        val input = "100"
+        val expectedValue = 100U * KOPEYKA
+
+        //act
+        val actualValue = getLastMonthSum(input)
+
+        //assert
+        assertEquals(expectedValue, actualValue)
+    }
+}
